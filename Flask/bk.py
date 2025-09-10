9b650ddb943d003705198982bba2206ac4c02fb1
Stamina 💙 <stamsprogramming@gmail.com>
2025-09-10 13:40:07 +0100

    Update bk.py

--- Flask/bk.py
+++ Flask/bk.py
@@ -1,1 +1,18 @@
 
+from flask import Flask, render_template
+app=Flask(__name__)
+
+@app.route("/")
+def user():
+	return render_template("usercreate.html")
+	
+@app.route("/regform")
+def create():
+	return render_template("usercreate.html")
+	
+@app.route("/logform")	
+def log():
+	return render_template("lo.html")
+	
+if __name__ == "__main__":
+	app.run(debug=True, port=5007)
